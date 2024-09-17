d129a512dae206d0ccf8783e339edeec3a5a94a5
tbatchelder <pegacorn@optonline.net>
2024-09-17 19:08:11 -0400

    Initial Hello World example

---
+++ project-4a/src/Message.tsx
@@ -0,0 +1,10 @@
+// PacalCasing - capitalize first letter of every word
+function Message() {
+  // JSX: Javascript XML
+  return <h1>Hello Baby and Emblem!</h1>
+}
+
+// In order to use this, we must export it.
+// Note that file name = function name = export name
+// Called from App.tsx
+export default Message;
