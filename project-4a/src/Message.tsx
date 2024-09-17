5a6069b50720297d70a81ef6278b05cb386fec18
tbatchelder <pegacorn@optonline.net>
2024-09-17 19:21:42 -0400

    First if statement and const

--- project-4a/src/Message.tsx
+++ project-4a/src/Message.tsx
@@ -1,7 +1,11 @@
 // PacalCasing - capitalize first letter of every word
 function Message() {
   // JSX: Javascript XML
-  return <h1>Hello Baby and Emblem!</h1>
+  const name = "Stormfire"; // make line 7 work
+  //const name = ""; // Make like 8 work
+  if (name)
+    return <h1>Hello {name}, it's Baby and Emblem!</h1>
+  return <h1>Hellow Matrix, our guardian.</h1>
 }
 
 // In order to use this, we must export it.
