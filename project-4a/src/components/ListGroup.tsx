19143b8d3f51f06f972f71f22b4217f93faa5255
tbatchelder <pegacorn@optonline.net>
2024-09-20 18:53:43 -0400

    Conditional statements in components

--- project-4a/src/components/ListGroup.tsx
+++ project-4a/src/components/ListGroup.tsx
@@ -1,9 +1,32 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "Lodon", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "Lodon", "Paris"];
+  items = [];
+  // if (items.length == 0)
+  //   return (
+  //     <>
+  //       <p>No item found.</p>
+  //     </>
+  //   );
+
+  // const message = items.length == 0 ? <p>No items found</p> : null;
+
+  // const getMessage = () => {
+  //   return items.length == 0 ? <p>No items found</p> : null;
+  // };
 
   return (
     <>
       <h1>List Group</h1>
+      {/* {items.length == 0 ? <p>No items found</p> : null} */}
+      {/* {message} */
+      /* goes with the const message above*/}
+      {/* {getMessage()} */
+      /*goes with the const function above */}
+      {
+        items.length == 0 && (
+          <p>No item found.</p>
+        ) /* This removes the null and makes it more concise.  It works by saying if length = 0, then it is true and the string after && is always true so it prints the element.  If length is not 0, then it is false and prints nothing*/
+      }
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
